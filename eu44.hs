d5532494e8bb0120bc5970cd9dfccaffd099eaf5
Andreas Brandt <andreas.brandt.de@googlemail.com>
2015-03-14 23:41:22 +0100

    Added problem 44

--- eu44.hs
+++ eu44.hs
@@ -1,27 +1,20 @@
-pnumbers :: [Float]
-pnumbers = map (fromIntegral . p) [24000..25000]
+import Data.List (sort)
 
 pnumberss :: [Float]
-pnumberss = map (fromIntegral . p) [1..400]
+pnumberss = map (fromIntegral . p) [1..4000] -- 4000 was just a guess
 
 p n = n * (3 * n - 1) `div` 2
-
--- ohoh x = map (\x -> snd x - fst x) x
 
 isPent :: Float -> Bool
 isPent x | (snd $ properFraction  $ (sqrt (24*x+1) + 1) / 6) == 0.0 = True
          | otherwise = False
     
-
 test :: [Float]
-test = [(y-x) | x <- pnumbers, 
-                y <- filter (>x) pnumbers, 
---                z <- filter (>y) pnumbers, 
---                x+y==z,
+test = [(y-x) | x <- pnumberss,
+                y <- filter (>x) pnumberss,
                 isPent (y-x),
                 isPent (x+y)
                 ]
 
-run = filter (<5408352) test                
-
-main = return run
+main :: IO ()
+main = print $ head $ sort test
